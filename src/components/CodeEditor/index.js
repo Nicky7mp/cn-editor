0a36cb62cbc291dc3f4e596c036e5b897ce1d9a8
Lewis <lspencer712@gmail.com>
2019-09-18 19:36:35 +0100

    Routing Update

---
+++ src/components/CodeEditor/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CodeEditor = () => (
+    <div>
+      <h1>CodeEditor</h1>
+    </div>
+  );
+
+export default CodeEditor;
---
+++ src/components/Firebase/index.js
@@ -0,0 +1,9 @@
+import React from 'React';
+
+const Firebase = () => (
+    <div>
+      <h1>Firebase</h1>
+    </div>
+  );
+
+export default Firebase;
